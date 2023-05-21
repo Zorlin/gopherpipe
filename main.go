b6e2e6e5f120cb17fec1084fed5c0546dbbdb67d
Benjamin Arntzen <zorlin@gmail.com>
2023-05-21 17:51:39 +0800

    Seriously wut

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"bufio"
+	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"fmt"
 	"io"
 	"os"
 
-	kcp "github.com/xtaci/kcp-go"
+	quic "github.com/lucas-clemente/quic-go"
 )
 
 const (
@@ -19,53 +22,73 @@ var (
 	debug      = flag.Bool("debug", false, "enable debug mode")
 	port       = flag.String("port", "51115", "port number to use")
 	address    = flag.String("addr", "", "address for client mode")
+	certFile   = flag.String("cert", "", "certificate file")
+	keyFile    = flag.String("key", "", "private key file")
 )
 
 func main() {
 	flag.Parse()
 
-	blockCrypt, err := kcp.NewNoneBlockCrypt(nil) // Disable encryption
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Failed to create block crypt:", err)
-		return
+	tlsConfig := &tls.Config{
+		NextProtos: []string{"quic-echo-example"},
+		VerifyPeerCertificate: func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
+			return nil // Always return nil, to skip certificate validation
+		},
+	}
+
+	if *certFile != "" && *keyFile != "" {
+		cert, err := tls.LoadX509KeyPair(*certFile, *keyFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Failed to load certificate and key:", err)
+			return
+		}
+		tlsConfig.Certificates = []tls.Certificate{cert}
+	} else {
+		tlsConfig.InsecureSkipVerify = true // Disable encryption
 	}
 
 	if *serverMode {
-		startServer(*port, *debug, blockCrypt)
+		startServer(*port, *debug, tlsConfig)
 	} else {
-		startClient(*address, *debug, blockCrypt)
+		startClient(*address, *debug, tlsConfig)
 	}
 }
 
-func startServer(port string, debug bool, block kcp.BlockCrypt) {
-	listener, err := kcp.ListenWithOptions(":"+port, block, 10, 3)
+func startServer(port string, debug bool, tlsConfig *tls.Config) {
+	listener, err := quic.ListenAddr(":"+port, tlsConfig, nil)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to start server:", err)
 		return
 	}
-	fmt.Fprintf(os.Stderr, "Now listening for KCP connections on 0.0.0.0:%s\n", port)
+	fmt.Fprintf(os.Stderr, "Now listening for QUIC connections on 0.0.0.0:%s\n", port)
 
 	for {
-		conn, err := listener.AcceptKCP()
+		session, err := listener.Accept(context.Background())
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Failed to accept connection:", err)
 			continue
 		}
-		fmt.Fprintf(os.Stderr, "Accepted connection from %s\n", conn.RemoteAddr())
+		fmt.Fprintf(os.Stderr, "Accepted connection from %s\n", session.RemoteAddr())
 
-		go handleConnection(conn, debug)
+		go handleConnection(session, debug)
 	}
 }
 
-func handleConnection(conn *kcp.UDPSession, debug bool) {
-	defer conn.Close()
+func handleConnection(session quic.Session, debug bool) {
+	defer session.CloseWithError(0, "")
+
+	stream, err := session.AcceptStream(context.Background())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to accept stream:", err)
+		return
+	}
 
 	buffer := make([]byte, bufferSize)
 	for {
-		n, err := conn.Read(buffer)
+		n, err := stream.Read(buffer)
 		if err != nil {
 			if err != io.EOF {
-				fmt.Fprintf(os.Stderr, "Connection with %s failed: %s\n", conn.RemoteAddr(), err)
+				fmt.Fprintf(os.Stderr, "Connection with %s failed: %s\n", session.RemoteAddr(), err)
 			}
 			return
 		}
@@ -81,38 +104,44 @@ func handleConnection(conn *kcp.UDPSession, debug bool) {
 	}
 }
 
-func startClient(addr string, debug bool, block kcp.BlockCrypt) {
-    conn, err := kcp.DialWithOptions(addr, block, 10, 3)
-    if err != nil {
-        fmt.Fprintln(os.Stderr, "Failed to connect:", err)
-        return
-    }
-    defer conn.Close()
+func startClient(addr string, debug bool, tlsConfig *tls.Config) {
+	session, err := quic.DialAddr(addr, tlsConfig, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to connect:", err)
+		return
+	}
+	defer session.CloseWithError(0, "")
 
-    reader := bufio.NewReader(os.Stdin)
-    buffer := make([]byte, bufferSize)
+	stream, err := session.OpenStreamSync(context.Background())
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to open stream:", err)
+		return
+	}
 
-    go func() {
-        for {
-            n, err := reader.Read(buffer)
-            if err != nil {
-                if err != io.EOF {
-                    fmt.Fprintln(os.Stderr, "Failed to read from stdin:", err)
-                }
-                return
-            }
+	reader := bufio.NewReader(os.Stdin)
+	buffer := make([]byte, bufferSize)
 
-            if debug {
-                fmt.Fprintf(os.Stderr, "Read data: %s\n", string(buffer[:n]))
-            }
+	go func() {
+		for {
+			n, err := reader.Read(buffer)
+			if err != nil {
+				if err != io.EOF {
+					fmt.Fprintln(os.Stderr, "Failed to read from stdin:", err)
+				}
+				return
+			}
 
-            _, err = conn.Write(buffer[:n])
-            if err != nil {
-                fmt.Fprintf(os.Stderr, "Failed to send data: %s, retrying...\n", err)
-            }
-        }
-    }()
+			if debug {
+				fmt.Fprintf(os.Stderr, "Read data: %s\n", string(buffer[:n]))
+			}
 
-    // Keep the main goroutine running until the child goroutine finishes.
-    select {}
+			_, err = stream.Write(buffer[:n])
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Failed to send data: %s, retrying...\n", err)
+			}
+		}
+	}()
+
+	// Keep the main goroutine running until the child goroutine finishes.
+	select {}
 }
