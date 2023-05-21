4ec3f79bfd59ad4c32e32c59d325a5c175a3f872
Benjamin Arntzen <zorlin@gmail.com>
2023-05-22 07:25:00 +0800

    confusing...

--- main.go
+++ main.go
@@ -16,18 +16,15 @@ import (
 	quic "github.com/lucas-clemente/quic-go"
 )
 
-const (
-	bufferSize = 134217728 // 128 MB
-)
-
 var (
-	serverMode = flag.Bool("server", false, "run in server mode")
-	debug      = flag.Bool("debug", false, "enable debug mode")
-	port       = flag.String("port", "51115", "port number to use")
-	address    = flag.String("addr", "", "address for client mode")
-	certFile   = flag.String("cert", "", "certificate file")
-	keyFile    = flag.String("key", "", "private key file")
-	chanNum    = flag.Int("channels", 8, "number of parallel channels")
+	serverMode  = flag.Bool("server", false, "run in server mode")
+	debug       = flag.Bool("debug", false, "enable debug mode")
+	port        = flag.String("port", "51115", "port number to use")
+	address     = flag.String("addr", "", "address for client mode")
+	certFile    = flag.String("cert", "", "certificate file")
+	keyFile     = flag.String("key", "", "private key file")
+	chanNum     = flag.Int("channels", 8, "number of parallel channels")
+	bufferSize  = flag.Int("bufferSize", 134217728, "buffer size in bytes")
 )
 
 type channelData struct {
@@ -68,7 +65,7 @@ func main() {
 	tlsConfig := &tls.Config{
 		NextProtos: []string{"quic-echo-example"},
 		VerifyPeerCertificate: func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
-			return nil // Always return nil, to skip certificate validation
+			return nil // Always return nil to skip certificate validation
 		},
 	}
 
@@ -84,13 +81,13 @@ func main() {
 	}
 
 	if *serverMode {
-		startServer(*port, *debug, tlsConfig, *chanNum)
+		startServer(*port, *debug, tlsConfig, *chanNum, *bufferSize)
 	} else {
-		startClient(*address, *debug, tlsConfig, *chanNum)
+		startClient(*address, *debug, tlsConfig, *chanNum, *bufferSize)
 	}
 }
 
-func startServer(port string, debug bool, tlsConfig *tls.Config, chanNum int) {
+func startServer(port string, debug bool, tlsConfig *tls.Config, chanNum, bufferSize int) {
 	listener, err := quic.ListenAddr(":"+port, tlsConfig, nil)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to start server:", err)
@@ -106,11 +103,11 @@ func startServer(port string, debug bool, tlsConfig *tls.Config, chanNum int) {
 		}
 		fmt.Fprintf(os.Stderr, "Accepted connection from %s\n", session.RemoteAddr())
 
-		go handleConnection(session, debug, chanNum)
+		go handleConnection(session, debug, chanNum, bufferSize)
 	}
 }
 
-func handleConnection(session quic.Session, debug bool, chanNum int) {
+func handleConnection(session quic.Session, debug bool, chanNum, bufferSize int) {
 	defer session.CloseWithError(0, "")
 
 	var wg sync.WaitGroup
@@ -118,7 +115,7 @@ func handleConnection(session quic.Session, debug bool, chanNum int) {
 
 	for i := 0; i < chanNum; i++ {
 		wg.Add(1)
-		go handleStream(i, session, debug, &wg, chData)
+		go handleStream(i, session, debug, &wg, chData, bufferSize)
 	}
 
 	go handleWrite(chData)
@@ -127,137 +124,116 @@ func handleConnection(session quic.Session, debug bool, chanNum int) {
 	close(chData)
 }
 
-func handleStream(id int, session quic.Session, debug bool, wg *sync.WaitGroup, chData chan<- channelData) {
-    defer wg.Done()
-
-    stream, err := session.AcceptStream(context.Background())
-    if err != nil {
-        fmt.Fprintln(os.Stderr, "Failed to accept stream:", err)
-        return
-    }
-
-	for {
-		buffer := make([]byte, bufferSize+8) // Reset the buffer before each read
-		n, err := stream.Read(buffer)
-        if err != nil {
-            if err != io.EOF {
-                fmt.Fprintf(os.Stderr, "Connection with %s failed: %s\n", session.RemoteAddr(), err)
-            }
-            return
-        }
-
-        if debug {
-            fmt.Fprintf(os.Stderr, "Received data: %s\n", string(buffer[8:n])) // We're only printing the actual data, not the order value
-        }
-
-        order := binary.BigEndian.Uint64(buffer[:8]) // We're extracting the order value from the beginning of the buffer
-
-        chData <- channelData{
-            data:  buffer[8:n],
-            order: int64(order),
-        }
-    }
-}
-
-func handleWrite(chData <-chan channelData) {
-    priorityQueue := make(PriorityQueue, 0, bufferSize)
-    heap.Init(&priorityQueue)
-    var lastOrder int64 = -1
-
-    for data := range chData {
-        item := &data
-        heap.Push(&priorityQueue, item)
-        for priorityQueue.Len() > 0 && priorityQueue[0].order == lastOrder+1 {
-            lastOrder++
-            item := heap.Pop(&priorityQueue).(*channelData)
-            _, err := os.Stdout.Write(item.data)
-            if err != nil {
-                fmt.Fprintf(os.Stderr, "Failed to write data to stream: %s\n", err)
-            }
-        }
-    }
-}
-
-func readFull(reader io.Reader, buffer []byte) (int, error) {
-    var total int
-    for total < len(buffer) {
-        n, err := reader.Read(buffer[total:])
-        if n == 0 || err != nil {
-            return total, err
-        }
-        total += n
-    }
-    return total, nil
-}
-func startClient(addr string, debug bool, tlsConfig *tls.Config, chanNum int) {
-    session, err := quic.DialAddr(addr, tlsConfig, nil)
-    if err != nil {
-        fmt.Fprintln(os.Stderr, "Failed to connect:", err)
-        return
-    }
-    defer session.CloseWithError(0, "")
-
-    streams := make([]quic.Stream, chanNum)
-    for i := 0; i < chanNum; i++ {
-        stream, err := session.OpenStreamSync(context.Background())
-        if err != nil {
-            fmt.Fprintln(os.Stderr, "Failed to open stream:", err)
-            return
-        }
-        streams[i] = stream
-    }
-
-    reader := bufio.NewReader(os.Stdin)
-    buffer := make([]byte, bufferSize-8) // Leave room for the order value
-
-    var order int64 = 0
-    for {
-        n, err := readFull(reader, buffer)
-        if err != nil {
-            if err != io.EOF {
-                fmt.Fprintln(os.Stderr, "Failed to read from stdin:", err)
-            }
-            return
-        }
-
-        orderBytes := make([]byte, 8)
-        binary.BigEndian.PutUint64(orderBytes, uint64(order))
-        data := append(orderBytes, buffer[:n]...)
-
-        stream := streams[order%int64(chanNum)]
-
-        if debug {
-            fmt.Fprintf(os.Stderr, "Read data: %s\n", string(data[8:]))
-        }
-
-        _, err = stream.Write(data)
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "Failed to send data: %s, retrying...\n", err)
-        }
-
-        order++
-    }
-}
-
-func handleClientConnection(id int, session quic.Session, debug bool, wg *sync.WaitGroup, chData <-chan channelData) {
+func handleStream(id int, session quic.Session, debug bool, wg *sync.WaitGroup, chData chan<- channelData, bufferSize int) {
 	defer wg.Done()
 
-	stream, err := session.OpenStreamSync(context.Background())
+	stream, err := session.AcceptStream(context.Background())
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Failed to open stream:", err)
+		fmt.Fprintln(os.Stderr, "Failed to accept stream:", err)
 		return
 	}
 
-	for chData := range chData {
-		if chData.order%int64(*chanNum) == int64(id) {
-			if debug {
-				fmt.Fprintf(os.Stderr, "Read data: %s\n", string(chData.data))
-			}
+	buffer := make([]byte, bufferSize+8) // Create the buffer with the specified bufferSize
 
-			_, err = stream.Write(chData.data)
+	for {
+		n, err := stream.Read(buffer)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Fprintf(os.Stderr, "Connection with %s failed: %s\n", session.RemoteAddr(), err)
+			}
+			return
+		}
+
+		if debug {
+			fmt.Fprintf(os.Stderr, "Received data: %s\n", string(buffer[8:n])) // Print the actual data, excluding the order value
+		}
+
+		order := binary.BigEndian.Uint64(buffer[:8]) // Extract the order value from the buffer
+
+		chData <- channelData{
+			data:  buffer[8:n],
+			order: int64(order),
+		}
+	}
+}
+
+func handleWrite(chData <-chan channelData) {
+	priorityQueue := make(PriorityQueue, 0)
+	heap.Init(&priorityQueue)
+	var lastOrder int64 = -1
+
+	for data := range chData {
+		item := &data
+		heap.Push(&priorityQueue, item)
+		for priorityQueue.Len() > 0 && priorityQueue[0].order == lastOrder+1 {
+			lastOrder++
+			item := heap.Pop(&priorityQueue).(*channelData)
+			_, err := os.Stdout.Write(item.data)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "Failed to send data: %s, retrying...\n", err)
+				fmt.Fprintf(os.Stderr, "Failed to write data to stream: %s\n", err)
 			}
 		}
+	}
+}
+
+func readFull(reader io.Reader, buffer []byte) (int, error) {
+	var total int
+	for total < len(buffer) {
+		n, err := reader.Read(buffer[total:])
+		if n == 0 || err != nil {
+			return total, err
+		}
+		total += n
+	}
+	return total, nil
+}
+
+func startClient(addr string, debug bool, tlsConfig *tls.Config, chanNum, bufferSize int) {
+	session, err := quic.DialAddr(addr, tlsConfig, nil)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to connect:", err)
+		return
+	}
+	defer session.CloseWithError(0, "")
+
+	streams := make([]quic.Stream, chanNum)
+	for i := 0; i < chanNum; i++ {
+		stream, err := session.OpenStreamSync(context.Background())
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Failed to open stream:", err)
+			return
+		}
+		streams[i] = stream
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	buffer := make([]byte, bufferSize-8) // Leave room for the order value
+
+	var order int64 = 0
+	for {
+		n, err := readFull(reader, buffer)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Fprintln(os.Stderr, "Failed to read from stdin:", err)
+			}
+			return
+		}
+
+		orderBytes := make([]byte, 8)
+		binary.BigEndian.PutUint64(orderBytes, uint64(order))
+		data := append(orderBytes, buffer[:n]...)
+
+		stream := streams[order%int64(chanNum)]
+
+		if debug {
+			fmt.Fprintf(os.Stderr, "Read data: %s\n", string(data[8:]))
+		}
+
+		_, err = stream.Write(data)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Failed to send data: %s, retrying...\n", err)
+		}
+
+		order++
 	}
 }
